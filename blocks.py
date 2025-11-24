8fbce6a2d0bfa5eef0017d8f59e0958bf425ebf1
Montezuma1973 <astrakerpayne@gmail.com>
2025-11-24 13:33:56 -0500

    Initial commit: Tetris game with audio

---
+++ blocks.py
@@ -0,0 +1,124 @@
+from colors import Colors
+import pygame
+
+class Block:
+    def __init__(self, id):
+        self.id = id
+        self.cells = {}
+        self.cell_size = 30
+        self.row_offset = 0
+        self.column_offset = 0
+        self.rotation_state = 0
+        self.colors = Colors.get_cell_colors()
+
+    def move(self, rows, columns):
+        self.row_offset += rows
+        self.column_offset += columns
+
+    def get_cell_positions(self):
+        tiles = self.cells[self.rotation_state]
+        moved_tiles = []
+        for position in tiles:
+            position = Position(position.row + self.row_offset, position.column + self.column_offset)
+            moved_tiles.append(position)
+        return moved_tiles
+
+    def rotate(self):
+        self.rotation_state += 1
+        if self.rotation_state == len(self.cells):
+            self.rotation_state = 0
+
+    def undo_rotation(self):
+        self.rotation_state -= 1
+        if self.rotation_state == -1:
+            self.rotation_state = len(self.cells) - 1
+
+    def draw(self, screen, offset_x=11, offset_y=11):
+        tiles = self.get_cell_positions()
+        for tile in tiles:
+            tile_rect = pygame.Rect(offset_x + tile.column * self.cell_size, 
+                                  offset_y + tile.row * self.cell_size, 
+                                  self.cell_size - 1, self.cell_size - 1)
+            pygame.draw.rect(screen, self.colors[self.id], tile_rect)
+
+class Position:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+class LBlock(Block):
+    def __init__(self):
+        super().__init__(id=1)
+        self.cells = {
+            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
+        }
+        self.move(0, 3)
+
+class JBlock(Block):
+    def __init__(self):
+        super().__init__(id=2)
+        self.cells = {
+            0: [Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
+        }
+        self.move(0, 3)
+
+class IBlock(Block):
+    def __init__(self):
+        super().__init__(id=3)
+        self.cells = {
+            0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
+            1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
+            2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
+        }
+        self.move(-1, 3)
+
+class OBlock(Block):
+    def __init__(self):
+        super().__init__(id=4)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
+            1: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
+            2: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)]
+        }
+        self.move(0, 4)
+
+class SBlock(Block):
+    def __init__(self):
+        super().__init__(id=5)
+        self.cells = {
+            0: [Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)],
+            2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
+            3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
+        self.move(0, 3)
+
+class TBlock(Block):
+    def __init__(self):
+        super().__init__(id=6)
+        self.cells = {
+            0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
+        self.move(0, 3)
+
+class ZBlock(Block):
+    def __init__(self):
+        super().__init__(id=7)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)]
+        }
+        self.move(0, 3)
